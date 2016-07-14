7bf9c3979627cd70525554332308416bbee5890c
Mauve3 <mauvemoonman@gmail.com>
2016-07-14 15:34:24 -0700

    something

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Vector3 ResetCamera;
+	private Vector3 Origin;
+	private Vector3 Diference;
+	private bool Drag=false;
+
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+
+	void Awake () {
+		GameObject cube = GameObject.Find("Cube");
+		if (cube) {
+			Vector3 newPost = new Vector3 (cube.transform.position.x + 10, cube.transform.position.y, cube.transform.position.z);
+			Instantiate(cube, newPost, cube.transform.rotation);
+		}
+
+	}
+
+	void Update () {
+		yaw += speedH * Input.GetAxis("Mouse X");
+		pitch -= speedV * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+	}
+
+	/*void Start () {
+		ResetCamera = Camera.main.transform.position;
+	}
+	void LateUpdate () {
+		if (Input.GetMouseButton (0)) {
+			Debug.Log ("Click");
+			Diference=(Camera.main.ScreenToWorldPoint (Input.mousePosition))- Camera.main.transform.position;
+			if (Drag==false){
+				Drag=true;
+				Origin=Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			}
+		} else {
+			Drag=false;
+		}
+		if (Drag==true){
+			Camera.main.transform.position = Origin-Diference;
+		}
+		//RESET CAMERA TO STARTING POSITION WITH RIGHT CLICK
+		if (Input.GetMouseButton (1)) {
+			Camera.main.transform.position=ResetCamera;
+		}
+	}*/
+}
